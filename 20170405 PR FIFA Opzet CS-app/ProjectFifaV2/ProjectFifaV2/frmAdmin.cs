e9f02f0e538f6c0b56e1790d7f24497109305f2a
Youssef el Jaddaoui <d18322@edu.rocwb.nl>
2017-04-21 10:32:50 +0200

    Upload C# application

---
+++ 20170405 PR FIFA Opzet CS-app/ProjectFifaV2/ProjectFifaV2/frmAdmin.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjectFifaV2
+{
+    public partial class frmAdmin : Form
+    {
+        private DatabaseHandler dbh;
+        private OpenFileDialog opfd;
+
+        DataTable table;
+
+        public frmAdmin()
+        {
+            dbh = new DatabaseHandler();
+            table = new DataTable();
+            this.ControlBox = false;
+            InitializeComponent();
+        }
+
+        private void btnAdminLogOut_Click(object sender, EventArgs e)
+        {
+            txtQuery.Text = null;
+            txtPath = null;
+            dgvAdminData.DataSource = null;
+            Hide();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            if (txtQuery.TextLength > 0)
+            {
+                ExecuteSQL(txtQuery.Text);
+            }
+        }
+
+        private void ExecuteSQL(string selectCommandText)
+        {
+            dbh.TestConnection();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(selectCommandText, dbh.GetCon());
+            dataAdapter.Fill(table);
+            dgvAdminData.DataSource = table;
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            txtPath.Text = null;
+            
+            string path = GetFilePath();
+
+            if (CheckExtension(path, "csv"))
+            {
+                txtPath.Text = path;
+            }
+            else
+            {
+                MessageHandler.ShowMessage("The wrong filetype is selected.");
+            }
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            if (!(txtPath.Text == null))
+            {
+                dbh.OpenConnectionToDB();
+
+                dbh.CloseConnectionToDB();
+            }
+            else
+            {
+                MessageHandler.ShowMessage("No filename selected.");
+            }
+        }
+        
+        private string GetFilePath()
+        {
+            string filePath = "";
+            opfd = new OpenFileDialog();
+
+            opfd.Multiselect = false;
+
+            if (opfd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = opfd.FileName;
+            }
+
+            return filePath;
+        }
+
+        private bool CheckExtension(string fileString, string extension)
+        {
+            int extensionLength = extension.Length;
+            int strLength = fileString.Length;
+
+            string ext = fileString.Substring(strLength - extensionLength, extensionLength);
+
+            if (ext == extension)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 20170405 PR FIFA Opzet CS-app/ProjectFifaV2/ProjectFifaV2/frmPlayer.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ProjectFifaV2
+{
+    partial class frmPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEditPrediction = new System.Windows.Forms.Button();
+            this.btnClearPrediction = new System.Windows.Forms.Button();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.lblResultsOverview = new System.Windows.Forms.Label();
+            this.btnShowRanking = new System.Windows.Forms.Button();
+            this.lvOverview = new System.Windows.Forms.ListView();
+            this.clmHomeTeam = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmHomeTeamScore = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmAwayTeamScore = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmAwayTeam = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pnlPredCard = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnEditPrediction
+            // 
+            this.btnEditPrediction.Location = new System.Drawing.Point(364, 70);
+            this.btnEditPrediction.Name = "btnEditPrediction";
+            this.btnEditPrediction.Size = new System.Drawing.Size(106, 30);
+            this.btnEditPrediction.TabIndex = 1;
+            this.btnEditPrediction.Text = "Edit Prediction";
+            this.btnEditPrediction.UseVisualStyleBackColor = true;
+            // 
+            // btnClearPrediction
+            // 
+            this.btnClearPrediction.Location = new System.Drawing.Point(364, 119);
+            this.btnClearPrediction.Name = "btnClearPrediction";
+            this.btnClearPrediction.Size = new System.Drawing.Size(106, 30);
+            this.btnClearPrediction.TabIndex = 2;
+            this.btnClearPrediction.Text = "Clear Prediction";
+            this.btnClearPrediction.UseVisualStyleBackColor = true;
+            this.btnClearPrediction.Click += new System.EventHandler(this.btnClearPrediction_Click);
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.Location = new System.Drawing.Point(364, 173);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(106, 30);
+            this.btnLogOut.TabIndex = 3;
+            this.btnLogOut.Text = "Log Out";
+            this.btnLogOut.UseVisualStyleBackColor = true;
+            this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
+            // 
+            // lblResultsOverview
+            // 
+            this.lblResultsOverview.AutoSize = true;
+            this.lblResultsOverview.Location = new System.Drawing.Point(582, 20);
+            this.lblResultsOverview.Name = "lblResultsOverview";
+            this.lblResultsOverview.Size = new System.Drawing.Size(90, 13);
+            this.lblResultsOverview.TabIndex = 5;
+            this.lblResultsOverview.Text = "Results Overview";
+            // 
+            // btnShowRanking
+            // 
+            this.btnShowRanking.Location = new System.Drawing.Point(364, 21);
+            this.btnShowRanking.Name = "btnShowRanking";
+            this.btnShowRanking.Size = new System.Drawing.Size(106, 30);
+            this.btnShowRanking.TabIndex = 6;
+            this.btnShowRanking.Text = "Show Ranking";
+            this.btnShowRanking.UseVisualStyleBackColor = true;
+            this.btnShowRanking.Click += new System.EventHandler(this.btnShowRanking_Click);
+            // 
+            // lvOverview
+            // 
+            this.lvOverview.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmHomeTeam,
+            this.clmHomeTeamScore,
+            this.clmAwayTeamScore,
+            this.clmAwayTeam});
+            this.lvOverview.Location = new System.Drawing.Point(476, 36);
+            this.lvOverview.Name = "lvOverview";
+            this.lvOverview.Size = new System.Drawing.Size(310, 600);
+            this.lvOverview.TabIndex = 7;
+            this.lvOverview.UseCompatibleStateImageBehavior = false;
+            this.lvOverview.View = System.Windows.Forms.View.Details;
+            // 
+            // clmHomeTeam
+            // 
+            this.clmHomeTeam.Text = "HomeTeam";
+            this.clmHomeTeam.Width = 100;
+            // 
+            // clmHomeTeamScore
+            // 
+            this.clmHomeTeamScore.Text = "Score";
+            this.clmHomeTeamScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.clmHomeTeamScore.Width = 50;
+            // 
+            // clmAwayTeamScore
+            // 
+            this.clmAwayTeamScore.Text = "Score";
+            this.clmAwayTeamScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.clmAwayTeamScore.Width = 50;
+            // 
+            // clmAwayTeam
+            // 
+            this.clmAwayTeam.Text = "Away Team";
+            this.clmAwayTeam.Width = 100;
+            // 
+            // pnlPredCard
+            // 
+            this.pnlPredCard.Location = new System.Drawing.Point(12, 36);
+            this.pnlPredCard.Name = "pnlPredCard";
+            this.pnlPredCard.Size = new System.Drawing.Size(346, 599);
+            this.pnlPredCard.TabIndex = 8;
+            // 
+            // frmPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.pnlPredCard);
+            this.Controls.Add(this.lvOverview);
+            this.Controls.Add(this.btnShowRanking);
+            this.Controls.Add(this.lblResultsOverview);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.btnClearPrediction);
+            this.Controls.Add(this.btnEditPrediction);
+            this.Name = "frmPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PlayerName";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEditPrediction;
+        private System.Windows.Forms.Button btnClearPrediction;
+        private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.Label lblResultsOverview;
+        private System.Windows.Forms.Button btnShowRanking;
+        private System.Windows.Forms.ListView lvOverview;
+        private System.Windows.Forms.ColumnHeader clmHomeTeam;
+        private System.Windows.Forms.ColumnHeader clmHomeTeamScore;
+        private System.Windows.Forms.ColumnHeader clmAwayTeamScore;
+        private System.Windows.Forms.ColumnHeader clmAwayTeam;
+        private System.Windows.Forms.Panel pnlPredCard;
+    }
+}
